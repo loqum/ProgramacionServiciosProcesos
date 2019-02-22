a32a71e7a50483092e9e85f85a9620b5a459c756
loqum <ruben693@gmail.com>
2019-02-22 21:05:20 +0100

    Final commit.

--- ProgramacionServiciosProcesosUF01AC01/src/main/java/com/rfm/utils/CrearArchivo.java
+++ ProgramacionServiciosProcesosUF01AC01/src/main/java/com/rfm/utils/CrearArchivo.java
@@ -4,6 +4,10 @@ import java.io.File;
 
 public class CrearArchivo {
 
+  private CrearArchivo() {
+    throw new IllegalStateException("Utility class");
+  }
+
   public static File crear(String nombreArchivo) {
     return new File(nombreArchivo);
   }
